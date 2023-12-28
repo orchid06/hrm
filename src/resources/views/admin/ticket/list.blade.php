0ae5ea9da15a620b1037ebff7af85fececd6e2de
Mokaram-Hossain <mokarram254@gmail.com>
2023-12-28 17:38:21 +0600

    [hotfix-backenddesign] application updated update

--- src/resources/views/admin/ticket/list.blade.php
+++ src/resources/views/admin/ticket/list.blade.php
@@ -251,28 +251,39 @@
         <h4 class="card-title">Update Application</h4>
     </div>
     <div class="card-body">
-        <div class="d-flex align-items-center flex-md-nowrap flex-wrap gap-3 mb-4">
-            <div class="version">
-                <span>Current Version</span>
-                <h4>v4.0.2</h4>
-                <p>2023-12-18 09:45:23</p>
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="d-flex align-items-center flex-md-nowrap flex-wrap gap-3 mb-4">
+                    <div class="version">
+                        <span>Current Version</span>
+                        <h4>v4.0.2</h4>
+                        <p>2023-12-18 09:45:23</p>
+                    </div>
+                    <div class="version latest">
+                        <span>Latest Version</span>
+                        <h4>v4.0.2</h4>
+                        <p>2023-12-18 09:45:23</p>
+                    </div>
+             </div>
+                <div class="mt-4 mb-4">
+                    <label  for="image" class="feedback-file">
+                        <input hidden  data-size = "100x100" type="file" name="image" id="image" class="preview">
+                        <span><i class="bi bi-file-zip"></i>
+                            {{translate("Update file")}}
+                        </span>
+                    </label>
+                    <div class="image-preview-section">
+                    </div>
+                </div>
+                <button class="i-btn btn--lg btn--primary update-btn" type="submit">Update Now</button>
             </div>
-            <div class="version latest">
-                <span>Latest Version</span>
-                <h4>v4.0.2</h4>
-                <p>2023-12-18 09:45:23</p>
-            </div>
-        </div>
-        <button class="i-btn btn--lg btn--primary update-btn" type="submit">Update Now <i class="bi bi-arrow-repeat ms-2"></i></button>
-
-        <div class="mt-5">
-            <label  for="image" class="feedback-file">
-                <input hidden  data-size = "100x100" type="file" name="image" id="image" class="preview">
-                <span><i class="bi bi-file-zip"></i>
-                    {{translate("Update file")}}
-                </span>
-            </label>
-            <div class="image-preview-section">
+            <div class="col-lg-6">
+                <div class="text-center">
+                    <div class="update-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="512" height="512" x="0" y="0" viewBox="0 0 66 66" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><path d="M24.1 29.4c.3.2.8.1 1.1-.1L42.1 16c.2-.2.4-.5.4-.8s-.1-.6-.4-.8l-17-13.2c-.3-.2-.7-.3-1.1-.1-.3.2-.6.5-.6.9v4.4c-7.4.3-14 3.9-18.3 10.1C.7 22.8-.3 30.9 2.4 38.1c.1.4.5.7.9.7s.8-.3.9-.7c3-8.1 10.6-13.6 19.2-14v4.4c.1.4.3.8.7.9zM3.6 34.9C2.1 29 3.3 22.8 6.9 17.7c4.1-5.9 10.5-9.3 17.7-9.3.6 0 1-.4 1-1V4.1l14.3 11.2-14.4 11.2v-3.4c0-.6-.4-1-1-1-8.8 0-16.9 5.1-20.9 12.8zM63.5 27.9c-.1-.4-.5-.7-.9-.7s-.8.3-.9.7c-3 8.1-10.6 13.6-19.2 14v-4.4c0-.4-.2-.7-.6-.9-.3-.2-.8-.1-1.1.1L23.9 50c-.2.2-.4.5-.4.8s.1.6.4.8l16.9 13.3c.3.2.7.3 1.1.1.3-.2.6-.5.6-.9v-4.4c7.4-.3 14-3.9 18.3-10.1 4.4-6.5 5.4-14.5 2.7-21.7zm-4.4 20.4c-4.1 5.9-10.5 9.2-17.7 9.2-.6 0-1 .4-1 1v3.4L26.2 50.7l14.3-11.2v3.4c0 .6.4 1 1 1 8.9 0 17-5.1 20.9-12.8 1.4 5.9.3 12.2-3.3 17.2z"  opacity="1" data-original="#000000"></path></g></svg>
+                    </div>
+                    <button class="i-btn btn--lg btn--success update-btn" type="submit">One Click Update</button>
+                </div>
             </div>
         </div>
     </div>
