debad4a962d2d33e0a7e5bb6b3f1f74773ff88e3
nahfi <nahfi01@gmail.com>
2024-01-04 19:05:08 +0600

    some issues solved

--- src/routes/installer.php
+++ src/routes/installer.php
@@ -1,13 +1,14 @@
 <?php
 
 use App\Http\Controllers\InstallerController;
+use App\Http\Middleware\SoftwareVerification;
 use Illuminate\Support\Facades\Route;
 
 
 
 
     #Install er route
-    Route::controller(InstallerController::class)->prefix("/install")->name('install.')->group(function(){
+    Route::controller(InstallerController::class)->prefix("/install")->name('install.')->withoutMiddleware([SoftwareVerification::class,])->group(function(){
 
         Route::get('/','init')->name('init');
         Route::get('/requirement-verification','requirementVerification')->name('requirement.verification');
