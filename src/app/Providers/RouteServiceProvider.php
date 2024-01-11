5fad0038309992eca44dd4953b9bd8dfd6a837b9
Kamrul Islam <kmislam.kamrul@gmail.com>
2024-01-11 17:35:46 +0600

    [fix]profile page tab

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -30,7 +30,7 @@ class RouteServiceProvider extends ServiceProvider
     public function boot(): void
     {
 
-        // $this->configureRateLimiting();
+        $this->configureRateLimiting();
  
 
       
