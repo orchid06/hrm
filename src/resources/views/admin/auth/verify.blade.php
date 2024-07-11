3f696e913345c4019fe15d44d44f1402b12da4fd
Mokaram-Hossain <mokarram254@gmail.com>
2024-07-11 11:22:23 +0600

    auth

--- src/resources/views/admin/auth/verify.blade.php
+++ src/resources/views/admin/auth/verify.blade.php
@@ -4,15 +4,15 @@
 <div class="row form-area justify-content-center align-items-stretch g-0">
     <div class="col-lg-5">
         <div class="form-wrapper">
-            <div class="row mb-25 gy-5">
-                <div class="col-md-6 order-md-1 order-2 text-start">
+            <div class="row mb-25 gy-4">
+                <div class="col-md-12 text-center">
+                    <a href="{{route('admin.home')}}" class="site-logo">
+                        <img src="{{imageURL(@site_logo('site_logo')->file,'site_logo',true)}}" class="ms-md-auto me-md-0 mx-auto" alt="{{@site_logo('site_logo')->file->name}}">                    </a>
+                </div>
+                <div class="col-md-12">
                     <h4>
                         {{@translate($title)}}
                     </h4>
-                </div>
-                <div class="col-md-6 order-md-2 order-1 text-md-end text-center">
-                    <a href="{{route('admin.home')}}" class="site-logo">
-                        <img src="{{imageURL(@site_logo('site_logo')->file,'site_logo',true)}}" class="ms-md-auto me-md-0 mx-auto" alt="{{@site_logo('site_logo')->file->name}}">                    </a>
                 </div>
             </div>
             <form action="{{route('admin.password.verify.code')}}" class="login-right-form" method="post">
